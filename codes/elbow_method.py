ccd37b928b0372a60e23d1647a92a7295c1b8b01
Preetham Ganesh <preetham.ganesh2015@gmail.com>
2021-12-08 16:25:18 -0600

    changed data_combine, added data_preprocessing

--- codes/elbow_method.py
+++ codes/elbow_method.py
@@ -6,21 +6,39 @@
 
 import pandas as pd
 import os
+from data_preprocessing import min_max_normalization
 
 
-def data_combiner(district_names: list):
+def data_combine(district_names: list):
+    """Uses district_names to read the datasets and combine them into a single dataset.
+
+        Args:
+            district_names: List containing names of 29 districts in Tamil Nadu, India
+
+        Returns:
+            Pandas dataframe containing the combined data from all the 29 districts in Tamil Nadu, India
+    """
     directory_path = '../data/combined_data'
-    combined_dataframe = pd.read_csv('{}/{}'.format(directory_path, district_names[0]), sep='\t')
+    combined_dataframe = pd.read_csv('{}/{}'.format(directory_path, district_names[0]))
     for i in range(1, len(district_names)):
-        district_data = pd.read_csv('{}/{}'.format(directory_path, district_names[i]), sep='\t')
+        district_data = pd.read_csv('{}/{}'.format(directory_path, district_names[i]))
         combined_dataframe = combined_dataframe.append(district_data)
     return combined_dataframe
+
+
+def data_preprocessing(combined_dataframe: pd.DataFrame):
+    features = list(combined_dataframe.columns)
+    combined_min_max_dataframe = pd.DataFrame(columns=features)
+    for i in range(len(features)):
+        combined_min_max_dataframe[features[i]] = min_max_normalization(list(combined_dataframe[features[i]]))
+    return combined_min_max_dataframe
 
 
 def main():
     district_names = os.listdir('../data/combined_data')
     district_names.sort()
-    data_combiner(district_names)
+    combined_dataframe = data_combine(district_names)
+    combined_min_max_dataframe = data_preprocessing(combined_dataframe)
 
 
 if __name__ == '__main__':
