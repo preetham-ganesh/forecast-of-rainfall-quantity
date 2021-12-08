27cce1fc098b68d4d78a10b24fcaf57803eefae4
Preetham Ganesh <preetham.ganesh2015@gmail.com>
2021-12-08 16:38:22 -0600

    changed data_combine, changed data_preprocessing

--- codes/elbow_method.py
+++ codes/elbow_method.py
@@ -7,6 +7,7 @@
 import pandas as pd
 import os
 from data_preprocessing import min_max_normalization
+from data_preprocessing import district_data_export
 
 
 def data_combine(district_names: list):
@@ -23,14 +24,25 @@ def data_combine(district_names: list):
     for i in range(1, len(district_names)):
         district_data = pd.read_csv('{}/{}'.format(directory_path, district_names[i]))
         combined_dataframe = combined_dataframe.append(district_data)
+    district_data_export('all_districts', 'combined_data', combined_dataframe)
     return combined_dataframe
 
 
 def data_preprocessing(combined_dataframe: pd.DataFrame):
+    """Performs min-max normalization on the combined dataframe for all the features individually.
+
+        Args:
+            combined_dataframe: Pandas dataframe containing the combined data from all the 29 districts in Tamil Nadu,
+                                India
+        Returns:
+            Min-max normalized Pandas dataframe containing the combined data from all the 29 districts in Tamil Nadu,
+            India
+    """
     features = list(combined_dataframe.columns)
     combined_min_max_dataframe = pd.DataFrame(columns=features)
     for i in range(len(features)):
         combined_min_max_dataframe[features[i]] = min_max_normalization(list(combined_dataframe[features[i]]))
+    district_data_export('all_districts', 'min_max_normalized_data', combined_dataframe)
     return combined_min_max_dataframe
 
 
