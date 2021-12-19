5679b42a53507cc2c1ae38dd490d540aba850b18
Preetham Ganesh <preetham.ganesh2015@gmail.com>
2021-12-18 18:31:54 -0600

    added plot_preprocessing

--- codes/variation_analysis.py
+++ codes/variation_analysis.py
@@ -6,6 +6,9 @@
 
 import pandas as pd
 import numpy as np
+from scipy.interpolate import interp1d
+import matplotlib.pyplot as plt
+from matplotlib.pyplot import figure
 
 
 def compute_cluster_monthly_median(clusters: list,
@@ -25,14 +28,36 @@ def compute_cluster_monthly_median(clusters: list,
     return cluster_monthly_median_dataframe
 
 
-def plot_preprocessing(cluster_)
+def plot_preprocessing(cluster_month_median_dataframe: pd.DataFrame,
+                       clusters: list,
+                       months: list):
+    months_numbers = list(range(len(months)))
+    months_numbers_linspace = np.linspace(min(months_numbers), max(months_numbers), 500)
+    cluster_smooth_values = {}
+    cluster_smooth_values_mean = {}
+    for i in range(len(clusters)):
+        smoothing_function_current_cluster = interp1d(months_numbers, list(cluster_month_median_dataframe[clusters[i]]),
+                                                      kind='quadratic')
+        cluster_smooth_values[clusters[i]] = smoothing_function_current_cluster(months_numbers_linspace)
+        cluster_smooth_values_mean[clusters[i]] = [np.mean(cluster_smooth_values[clusters[i]]) for i in
+                                                   range(len(cluster_smooth_values[clusters[i]]))]
+    return cluster_smooth_values, cluster_smooth_values_mean
+
+
+def generate_plot(cluster_smooth_values: dict,
+                  cluster_smooth_values_mean: dict):
+    font = {'family': 'Times New Roman',
+            'size': 28}
+    plt.rc('font', **font)
+    figure(num=None, figsize=(20, 10))
+
 
 
 def main():
     clusters = ['cluster_{}'.format(str(i)) for i in range(0, 6)]
     months = ['jan', 'feb', 'mar', 'apr', 'may', 'jun', 'jul', 'aug', 'sep', 'oct', 'nov', 'dec']
     cluster_monthly_median_dataframe = compute_cluster_monthly_median(clusters, months)
-
+    plot_preprocessing(cluster_monthly_median_dataframe, clusters, months)
 
 
 
